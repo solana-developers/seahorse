ea32fe922b43f9d5770da24294439b22e1780ec0
Lost In Time <92709487+lostintime101@users.noreply.github.com>
2024-02-20 13:15:34 +0700

    modified hello example

--- examples/hello.py
+++ examples/hello.py
@@ -1,11 +1,11 @@
 # hello
-# Built with Seahorse v0.2.3
+# Built with Seahorse v0.2.0
 #
 # Greets users to Solana by printing a message and minting them a token!
 
 from seahorse.prelude import *
 
-declare_id('Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS')
+declare_id('')
 
 
 class Hello(Account):
@@ -13,7 +13,8 @@ class Hello(Account):
 
 
 @instruction
-def init(owner: Signer, hello: Empty[Hello], mint: Empty[TokenMint]):
+def init_token(owner: Signer, hello: Empty[Hello], mint: Empty[TokenMint]):
+  
   bump = hello.bump()
 
   hello = hello.init(
@@ -32,12 +33,26 @@ def init(owner: Signer, hello: Empty[Hello], mint: Empty[TokenMint]):
 
 
 @instruction
+def init_user_token_account(user: Signer, user_acc: Empty[TokenAccount], mint: TokenMint):
+    
+  user_acc.init(
+    payer = user,
+    seeds = ["hello-token", mint],
+    mint = mint,
+    authority = user
+  )
+
+
+@instruction
 def say_hello(user_acc: TokenAccount, hello: Hello, mint: TokenMint):
+
   bump = hello.bump
-  print(f'Hello {user_acc.authority()}, have a token!')
+  
   mint.mint(
     authority = hello,
     to = user_acc,
     amount = u64(1),
     signer = ['hello', bump]
   )
+
+  print(f'Hello {user_acc.authority()}, have a token!')
